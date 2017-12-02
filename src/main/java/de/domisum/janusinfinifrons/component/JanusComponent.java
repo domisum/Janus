66726d639c38f9795df432ca5d076b4bd952bdf7
Dominik <dominik.weissenseel@gmx.de>
2017-12-02 18:10:59 +0100

    Added base classes and storage interface

---
+++ src/main/java/de/domisum/janusinfinifrons/component/JanusComponent.java
@@ -0,0 +1,13 @@
+package de.domisum.janusinfinifrons.component;
+
+public interface JanusComponent
+{
+
+	void checkForUpdates();
+
+	boolean isUpdateAvailable();
+
+
+
+
+}
---
+++ src/main/java/de/domisum/janusinfinifrons/project/JanusProject.java
@@ -0,0 +1,13 @@
+package de.domisum.janusinfinifrons.project;
+
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class JanusProject
+{
+
+	private final String id;
+
+}
