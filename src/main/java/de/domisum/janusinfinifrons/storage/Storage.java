1f3eee1bb595cbc28ded68034b42c74cdf8d8762
Dominik <dominik.weissenseel@gmx.de>
2017-12-11 18:39:55 +0100

    Split storage interface to Source interface

--- src/main/java/de/domisum/janusinfinifrons/storage/Storage.java
+++ src/main/java/de/domisum/janusinfinifrons/storage/Storage.java
@@ -1,54 +1,25 @@
 package de.domisum.janusinfinifrons.storage;
 
-import java.util.Collection;
-
 /**
  * Interface describing the contract of a storage module. The purpose of the module is to store objects of the type
- * <code>StorageItemT</code> and be able to retrieve the instances later on. This contract does not guarantee persistence,
+ * <code>T</code> and be able to retrieve the instances later on. This contract does not guarantee persistence,
  * which could cause loss of stored data on shutdown. To check if this is the case, check the actual implementation of the
  * Storage module.
  * <p>
  * A storage module implementing this interface can only hold one item per id.
  *
- * @param <StorageItemT> the type of item to be stored
+ * @param <T> the type of item to be stored
  */
-public interface Storage<StorageItemT>
+public interface Storage<T> extends Source<T>
 {
 
 	/**
-	 * Fetches the item associated with the given id. Iff the storage does not contain an item with the
-	 * given id (iff {@link #contains(String)}</code> returns false), null is returned.
-	 *
-	 * @param id the id of the object to retrieve from storage
-	 * @return the StorageItemT associated with the id, or null if none found
-	 */
-	StorageItemT fetch(String id);
-
-	/**
-	 * Fetches a collection of all StorageItemT stored by this storage module. If no items are stored, an empty collection is
-	 * returned.
-	 *
-	 * @return a collection containing
-	 */
-	Collection<StorageItemT> fetchAll();
-
-
-	/**
-	 * Stores a <code>StorageItemT</code> in this module in order to fetch it later on. If the storage module already contains
-	 * another item with the same id, it is discarded and the new item stored in its place.
+	 * Stores a <code>T</code> in this module in order to fetch it later on. If the storage module already contains
+	 * another item with the same id, the old item is discarded and the new item stored in its place.
 	 *
 	 * @param item the item to store
 	 */
-	void store(StorageItemT item);
-
-	/**
-	 * Checks if this storage module contains a <code>StorageItemT</code> with the id equal to the supplied id.
-	 *
-	 * @param id the id of the item to check against
-	 * @return whether this storage module contains an item with the supplied id
-	 */
-	boolean contains(String id);
-
+	void store(T item);
 
 	/**
 	 * Removes the item with the supplied id from the storage module.
