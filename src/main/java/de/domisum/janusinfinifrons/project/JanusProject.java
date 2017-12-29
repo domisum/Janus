863769769d121e54b57c706139c18bb6ee6664d3
Dominik <dominik.weissenseel@gmx.de>
2017-12-29 20:00:01 +0100

    Added JanusProject component list validation

--- src/main/java/de/domisum/janusinfinifrons/project/JanusProject.java
+++ src/main/java/de/domisum/janusinfinifrons/project/JanusProject.java
@@ -3,6 +3,8 @@ package de.domisum.janusinfinifrons.project;
 import de.domisum.janusinfinifrons.component.JanusComponent;
 import de.domisum.lib.auxilium.contracts.Identifyable;
 import de.domisum.lib.auxilium.contracts.source.FiniteSource;
+import de.domisum.lib.auxilium.util.PHR;
+import de.domisum.lib.auxilium.util.java.exceptions.InvalidConfigurationException;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +22,9 @@ public class JanusProject implements Identifyable
 
 	public void validate(FiniteSource<String, JanusComponent> componentSource)
 	{
-
+		for(String cid : componentIds)
+			if(!componentSource.contains(cid))
+				throw new InvalidConfigurationException(PHR.r("project '{}' depends on non-existant component '{}'", id, cid));
 	}
 
 }
