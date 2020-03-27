34f2d55cb4bd6839ce1dd08414b22b23be5fba7d
domisum <dominik.weissenseel@gmail.com>
2020-03-27 20:12:56 +0100

    Added build log messages

--- src/main/java/io/domisum/janus/build/ProjectBuilder.java
+++ src/main/java/io/domisum/janus/build/ProjectBuilder.java
@@ -2,13 +2,24 @@ package io.domisum.janus.build;
 
 import io.domisum.janus.config.Configuration;
 import io.domisum.janus.config.object.project.Project;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class ProjectBuilder
 {
 	
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	
+	// BUILD
 	public void build(Project project, Configuration configuration)
 	{
-	
+		logger.info("Building project {}...", project.getId());
+		
+		// TODO
+		
+		logger.info("...Building project {} done", project.getId());
 	}
 	
 }
