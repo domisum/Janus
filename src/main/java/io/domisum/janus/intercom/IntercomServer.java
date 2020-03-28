664c61bd2279ff46705945ca402f819ad9741c80
domisum <dominik.weissenseel@gmail.com>
2020-03-28 01:58:32 +0100

    Separate Janus from JanusApi

--- src/main/java/io/domisum/janus/intercom/IntercomServer.java
+++ src/main/java/io/domisum/janus/intercom/IntercomServer.java
@@ -2,9 +2,8 @@ package io.domisum.janus.intercom;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import io.domisum.janus.api.JanusApiHttp;
-import io.domisum.lib.httpbutler.HttpButlerServer;
 import io.domisum.lib.httpbutler.HttpButlerEndpoint;
+import io.domisum.lib.httpbutler.HttpButlerServer;
 
 import java.util.Set;
 
@@ -13,11 +12,15 @@ public class IntercomServer
 		extends HttpButlerServer
 {
 	
+	// CONSTANTS
+	private static final int PORT = 8381;
+	
+	
 	// INIT
 	@Inject
 	public IntercomServer(Set<HttpButlerEndpoint> endpoints)
 	{
-		super("localhost", JanusApiHttp.PORT, endpoints);
+		super("localhost", PORT, endpoints);
 	}
 	
 }
